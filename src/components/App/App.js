914f93c4d01d5b3c581c72a9702543039fbe0faa
tomkjose <tomjosekunnumpurath@gmail.com>
2024-02-19 11:08:58 +0530

    Added : HashRouter

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import { Home, Landing, Login, Cart, Fof } from "../../pages/Index";
 import Navbar from "../Navbar/Navbar";
 import "./App.css";
